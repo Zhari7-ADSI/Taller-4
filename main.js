0fd3db411e5c98ca8df950b5b8f919278228d946
Zhari7 <meneseszharick29@gmail.com>
2022-09-30 18:40:28 -0500

    Ejer_6

--- main.js
+++ main.js
@@ -2,28 +2,25 @@ addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('form');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
-        
-        let sum = 0;
-        let a = 0;
-        let b = 0;
-        let c = 0;
 
-        let vent = document.getElementById("numero1").value;
+        let trabajadores = document.getElementById("numero1").value;
 
-        for (i=1; i<=vent; i++){
-            let valor = parseInt(prompt('Ingrese el valor de la venta ' +i+ ':'));
+        for (i = 1; i <= trabajadores; i++) {
+            let aumento = parseInt(prompt('cuantos años lleva trabajando en la empresa?'));
 
-            if( valor > 1000){
-                a = a + 1;
-            }
-            else if(1000 >= valor && valor >500){
-                b = b + 1;
-            }
-            else{
-                c = c + 1;
-            }
-            sum += valor;
-		}
-        alert('Ventas mayores a 1000: '+a+'\nVentas mayores a 500 menores o iguales a 1000:'+b+'\nVentas menores o iguales a 500: '+c+'\nMonto total: '+sum);
+            if (1 <= aumento && aumento <= 5) {
+                alert('su aumento es de 100 pesos');
+                }
+                else if (5 < aumento && aumento <= 10) {
+                    alert('su aumento es de 250 pesos');
+                }
+                else if (10 < aumento && aumento <= 20) {
+                    alert('su aumento es de 400 pesos');
+                }
+                else{
+                    alert('su aumento es de 550 pesos');
+                }
+            
+        }
     })
-})
+    })
