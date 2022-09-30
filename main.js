af325a45e3dd620becd72764a5bbf2c7252a3dda
Zhari7 <meneseszharick29@gmail.com>
2022-09-30 17:29:43 -0500

    Ejer_5

--- main.js
+++ main.js
@@ -3,14 +3,27 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
         
-        let primer = 10;
-        let mes = 20;
+        let sum = 0;
+        let a = 0;
+        let b = 0;
+        let c = 0;
 
-        alert('El valor 1 a pagar es: ' +primer+ ' pesos');
+        let vent = document.getElementById("numero1").value;
 
-        for (i=2; i<=20; i++){
-            primer = primer * 2,
-            alert('El mes '+i+', el valor a pagar es de: '+primer+ ' pesos');
-        }
+        for (i=1; i<=vent; i++){
+            let valor = parseInt(prompt('Ingrese el valor de la venta ' +i+ ':'));
+
+            if( valor > 1000){
+                a = a + 1;
+            }
+            else if(1000 >= valor && valor >500){
+                b = b + 1;
+            }
+            else{
+                c = c + 1;
+            }
+            sum += valor;
+		}
+        alert('Ventas mayores a 1000: '+a+'\nVentas mayores a 500 menores o iguales a 1000:'+b+'\nVentas menores o iguales a 500: '+c+'\nMonto total: '+sum);
     })
 })
