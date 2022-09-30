876d556df268f65c669fc85e1b797f09bbe8146d
Zhari7 <meneseszharick29@gmail.com>
2022-09-30 15:23:07 -0500

    Ejer_2

--- main.js
+++ main.js
@@ -3,12 +3,17 @@ addEventListener("DOMContentLoaded", (e) => {
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
 
-        let num = document.getElementById("numero1").value;
-        if(num > 0){
-            for(let i = 1; i < 11; i++){
-                let mult = num * i;
-                alert(num +' x '+ i + '='+ mult)
+        let num = 1;
+        
+        for (let i = 1; i <= 20; i++) {
+            alert(num);
+            if (i%2==0) {
+                num-=2;
+            }
+            else {
+                num+=4;
             }
         }
+        
     })
 })
