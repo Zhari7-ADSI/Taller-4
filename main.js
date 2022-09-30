e3fcb80e7171aaf52bb249164a9d6d6e8c4ddf10
Zhari7 <meneseszharick29@gmail.com>
2022-09-30 17:02:06 -0500

    Ejer_4

--- main.js
+++ main.js
@@ -2,31 +2,15 @@ addEventListener("DOMContentLoaded", (e) => {
     let formulario = document.getElementById('form');
     formulario.addEventListener("submit", (e) => {
         e.preventDefault();
+        
+        let primer = 10;
+        let mes = 20;
 
-        let menor = 0;
-        let mayor = 0; 
-        let encue = 0;
-        
-        let estudiantes= document.getElementById("numero1").value;
-        
-        for (let i = 1; i <= estudiantes; i++) {
-            let cal = parseInt(prompt('Estudiante #' +i+ ' Ingrese su calificación'));
-            
-            if (cal > mayor) {
-                mayor = cal;
-            }
-            else if (cal < menor) {
-                menor = cal;
-            }
-            else {
-                mayor = cal;
-                menor = cal;
-            }
-            encue += cal;
-            
+        alert('El valor 1 a pagar es: ' +primer+ ' pesos');
+
+        for (i=2; i<=20; i++){
+            primer = primer * 2,
+            alert('El mes '+i+', el valor a pagar es de: '+primer+ ' pesos');
         }
-        let promedio = encue / estudiantes;
-        
-        alert('Promedio de notas: '+promedio+'\nNota más alta: '+mayor+ '\nNota más baja: '+menor);
     })
 })
