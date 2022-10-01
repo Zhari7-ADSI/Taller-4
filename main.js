c11f1adeee3d61931dc029238f25f15dc72ae1a8
Zhari7 <meneseszharick29@gmail.com>
2022-09-30 20:13:04 -0500

    Ejer_9

--- main.js
+++ main.js
@@ -5,17 +5,26 @@ addEventListener("DOMContentLoaded", (e) => {
 
         let cant = document.getElementById("numero1").value;
 
-        for (i=1; i<=cant; i++){
+        let mayor = 500000;
+        const iva1 = 0.19;
 
-            let num1 = parseInt(prompt(i+'-'+cant+'\n Ingrese el primer número:'));
-            let num2 = parseInt(prompt('Ingrese el segundo número:'));
+        for (i = 1; i <= cant; i++) {
 
-            let sum = num1 + num2;
-            let rest = num1-num2;
-            let mult = num1 * num2;
-            let div = num1 / num2;
-            
-            alert('suma: '+sum+ '\n resta: '+rest+ '\n multiplicación: '+mult+'\n división:'+div)
+            let valor_ar = parseInt(prompt(i + '-' + cant + '\n Ingrese el valor del árticulo:'));
+            let unidad = parseInt(prompt('Unidades compradas:'));
+
+            let sub_tot = valor_ar * unidad;
+            let iva2 = sub_tot * iva1;
+            let tot_pag = sub_tot + iva2;
+
+            if (tot_pag > mayor) {
+                let des = tot_pag * 0.15;
+                let desc_tot = tot_pag - des;
+                alert('La compra superó los 500.000, /n ¡¡¡ Felicidades, obten tu descuento!!! \n Pago con IVA: ' + tot_pag + '\n Total a pagar: ' + desc_tot + ' GRACIAS POR SU COMPRA💜');
+            }
+            else {
+                alert('Subtotal: ' + sub_tot + '\n IVA: ' + iva1 + '\n Total a pagar: ' + tot_pag);
+            }
         }
     })
 })
